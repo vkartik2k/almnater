b02f1ad4df6b21c96dba8199a4c59a1a321291ae
vkartik2k <vkartik2k@gmail.com>
2020-02-15 10:06:07 +0530

    aplha 1

---
+++ portal/src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './styles/App.css';
+import Header from './components/Header'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+import Home from './components/Home'
+import Connect from './components/Connect'
+import Mock from './components/Mock'
+import Discuss from './components/Discuss'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ffffff',
+    },
+  },
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentModule : 'mock'
+    }
+  }
+
+  _onChangeModule = moduleName => this.setState({currentModule : moduleName})
+
+  render() {
+    let mainContent = <Home/>
+    if(this.state.currentModule === 'discuss') {
+      mainContent = <Discuss/>
+    }
+    if(this.state.currentModule === 'connect') {
+      mainContent = <Connect/>
+    }
+    if(this.state.currentModule === 'mock') {
+      mainContent = <Mock/>
+    }
+
+    return (
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Header current={this.state.currentModule} onChange={this._onChangeModule} />
+          {mainContent}
+        </div>
+      </ThemeProvider>
+    )
+  }
+
+}
+
+export default App;
---
+++ portal/src/components/Discuss.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Discuss.css';
+
+class Discuss extends React.Component {
+    render() {
+        return (
+            <div>
+                Discuss
+        </div>
+        )
+    }
+}
+
+export default Discuss;
